422cd9a1d13ef43503f964e4bb922df841ff1328
Senamile M <mthethwasenamile88@gmail.com>
2024-08-05 18:27:20 +0200

    Injected more weather data

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#weather-temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let descriptionElement = document.querySelector("#discription");
   let city = document.querySelector("#weather-app-city");
+  let humidity = document.querySelector("#humidity");
+  let windElement = document.querySelector("#windSpeed");
+  let timeElement = document.querySelector("#currentTime");
+  let date = new Date(response.data.time * 1000);
   city.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {

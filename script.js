abd00292f6734983ad54ad90182830c0ffbc1c8c
Senamile M <mthethwasenamile88@gmail.com>
2024-08-07 10:45:41 +0200

    Styled the weather app, updated JS and added background image

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ function updateWeather(response) {
   let date = new Date(response.data.time * 1000);
   city.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
+  descriptionElement.innerHTML = capitalizeFirstLetter(
+    response.data.condition.description
+  );
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   iconImage.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon"/>`;
+  getForecastData(response.data.city);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function formatDate(date) {
@@ -48,22 +55,42 @@ function searchForm(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecastData(city) {
+  let apiKey = "409of717057a0ce5t4fdc2f2b69b384a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` 
-          <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+         <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div>
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+            </div>
             <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temp"><strong>15°</strong></div>
-              <div class="weather-forecast-temp">9°</div>
+              <div class="weather-forecast-temp"><strong>${Math.round(
+                day.temperature.maximum
+              )}°</strong></div>
+              <div class="weather-forecast-temp">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
@@ -71,5 +98,4 @@ function displayForecast() {
 
 let searchFormEmelent = document.querySelector("#search-form");
 searchFormEmelent.addEventListener("submit", searchForm);
-searchCity("Paris");
-displayForecast();
+searchCity("Johannesburg");
